8e141c74186c5ce2cc0239939bee867c46ecdc35
skywolf <skywolf@skywolf.com>
2024-10-15 21:08:18 +0800

    7次修正自动请求日志

--- app/api/meta/route.ts
+++ app/api/meta/route.ts
@@ -1,7 +1,5 @@
-import { NextResponse } from 'next/server'
-
 export async function GET() {
-  return NextResponse.json({ message: 'keeping alive' })
+  return new Response('keeping alive')
 }
 // 测试 client
 // testClient()
