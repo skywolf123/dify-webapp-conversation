9a7e1e782352846e94c8fce11fcd04f5a1119a3f
skywolf <skywolf@skywolf.com>
2024-10-21 15:54:36 +0800

    2次修正AppInitialization

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -23,7 +23,6 @@ import { API_KEY, APP_ID, APP_INFO, isShowPrompt, promptTemplate } from '@/confi
 import type { Annotation as AnnotationType } from '@/types/log'
 import { addFileInfos, sortAgentSorts } from '@/utils/tools'
 
-import useInitApp from '@/app/components/AppInitialization'
 import ConversationManagement from '@/app/components/ConversationManagement'
 
 const Main: FC = () => {
@@ -118,7 +117,55 @@ const Main: FC = () => {
     />
   )
 
-  useInitApp(hasSetAppConfig, setAppUnavailable, setInited, setPromptConfig, setVisionConfig, setConversationList, setNewConversationInfo, setCurrConversationId, setIsUnknownReason)
+  // init
+  useEffect(() => {
+    if (!hasSetAppConfig) {
+      setAppUnavailable(true)
+      return
+    }
+    (async () => {
+      try {
+        const [conversationData, appParams] = await Promise.all([fetchConversations(), fetchAppParams()])
+
+        // handle current conversation id
+        const { data: conversations } = conversationData as { data: ConversationItem[] }
+        const _conversationId = getConversationIdFromStorage(APP_ID)
+        const isNotNewConversation = conversations.some(item => item.id === _conversationId)
+
+        // fetch new conversation info
+        const { user_input_form, opening_statement: introduction, file_upload, system_parameters }: any = appParams
+        setLocaleOnClient(APP_INFO.default_language, true)
+        setNewConversationInfo({
+          name: t('app.chat.newChatDefaultName'),
+          introduction,
+        })
+        const prompt_variables = userInputsFormToPromptVariables(user_input_form)
+        setPromptConfig({
+          prompt_template: promptTemplate,
+          prompt_variables,
+        } as PromptConfig)
+        setVisionConfig({
+          ...file_upload?.image,
+          image_file_size_limit: system_parameters?.system_parameters || 0,
+        })
+        setConversationList(conversations as ConversationItem[])
+
+        if (isNotNewConversation)
+          setCurrConversationId(_conversationId, APP_ID, false)
+
+        setInited(true)
+      }
+      catch (e: any) {
+        if (e.status === 404) {
+          setAppUnavailable(true)
+        }
+        else {
+          setIsUnknownReason(true)
+          setAppUnavailable(true)
+        }
+      }
+    })()
+  }, [])
 
   const [isResponding, { setTrue: setRespondingTrue, setFalse: setRespondingFalse }] = useBoolean(false)
   const [abortController, setAbortController] = useState<AbortController | null>(null)
