4fb937cfec16d6c76430d874d978b159fd0c85ff
skywolf <skywolf@skywolf.com>
2024-10-21 16:07:29 +0800

    还原index

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -23,8 +23,6 @@ import { API_KEY, APP_ID, APP_INFO, isShowPrompt, promptTemplate } from '@/confi
 import type { Annotation as AnnotationType } from '@/types/log'
 import { addFileInfos, sortAgentSorts } from '@/utils/tools'
 
-import ConversationManagement from '@/app/components/ConversationManagement'
-
 const Main: FC = () => {
   const { t } = useTranslation()
   const media = useBreakpoints()
@@ -81,9 +79,96 @@ const Main: FC = () => {
     setExistConversationInfo,
   } = useConversation()
 
+  const [conversationIdChangeBecauseOfNew, setConversationIdChangeBecauseOfNew, getConversationIdChangeBecauseOfNew] = useGetState(false)
+  const [isChatStarted, { setTrue: setChatStarted, setFalse: setChatNotStarted }] = useBoolean(false)
+  const handleStartChat = (inputs: Record<string, any>) => {
+    createNewChat()
+    setConversationIdChangeBecauseOfNew(true)
+    setCurrInputs(inputs)
+    setChatStarted()
+    // parse variables in introduction
+    setChatList(generateNewChatListWithOpenStatement('', inputs))
+  }
+  const hasSetInputs = (() => {
+    if (!isNewConversation)
+      return true
+
+    return isChatStarted
+  })()
+
+  const conversationName = currConversationInfo?.name || t('app.chat.newChatDefaultName') as string
+  const conversationIntroduction = currConversationInfo?.introduction || ''
+
+  const handleConversationSwitch = () => {
+    if (!inited)
+      return
+
+    // update inputs of current conversation
+    let notSyncToStateIntroduction = ''
+    let notSyncToStateInputs: Record<string, any> | undefined | null = {}
+    if (!isNewConversation) {
+      const item = conversationList.find(item => item.id === currConversationId)
+      notSyncToStateInputs = item?.inputs || {}
+      setCurrInputs(notSyncToStateInputs as any)
+      notSyncToStateIntroduction = item?.introduction || ''
+      setExistConversationInfo({
+        name: item?.name || '',
+        introduction: notSyncToStateIntroduction,
+      })
+    }
+    else {
+      notSyncToStateInputs = newConversationInputs
+      setCurrInputs(notSyncToStateInputs)
+    }
+
+    // update chat list of current conversation
+    if (!isNewConversation && !conversationIdChangeBecauseOfNew && !isResponding) {
+      fetchChatList(currConversationId).then((res: any) => {
+        const { data } = res
+        const newChatList: ChatItem[] = generateNewChatListWithOpenStatement(notSyncToStateIntroduction, notSyncToStateInputs)
+
+        data.forEach((item: any) => {
+          newChatList.push({
+            id: `question-${item.id}`,
+            content: item.query,
+            isAnswer: false,
+            message_files: item.message_files?.filter((file: any) => file.belongs_to === 'user') || [],
+
+          })
+          newChatList.push({
+            id: item.id,
+            content: item.answer,
+            agent_thoughts: addFileInfos(item.agent_thoughts ? sortAgentSorts(item.agent_thoughts) : item.agent_thoughts, item.message_files),
+            feedback: item.feedback,
+            isAnswer: true,
+            message_files: item.message_files?.filter((file: any) => file.belongs_to === 'assistant') || [],
+          })
+        })
+        setChatList(newChatList)
+      })
+    }
+
+    if (isNewConversation && isChatStarted)
+      setChatList(generateNewChatListWithOpenStatement())
+  }
+  useEffect(handleConversationSwitch, [currConversationId, inited])
+
+  const handleConversationIdChange = (id: string) => {
+    if (id === '-1') {
+      createNewChat()
+      setConversationIdChangeBecauseOfNew(true)
+    }
+    else {
+      setConversationIdChangeBecauseOfNew(false)
+    }
+    // trigger handleConversationSwitch
+    setCurrConversationId(id, APP_ID)
+    hideSidebar()
+  }
+
   /*
-* chat info. chat is under conversation.
-*/
+  * chat info. chat is under conversation.
+  */
   const [chatList, setChatList, getChatList] = useGetState<ChatItem[]>([])
   const chatListDomRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
@@ -93,29 +178,40 @@ const Main: FC = () => {
   }, [chatList, currConversationId])
   // user can not edit inputs if user had send message
   const canEditInputs = !chatList.some(item => item.isAnswer === false) && isNewConversation
+  const createNewChat = () => {
+    // if new chat is already exist, do not create new chat
+    if (conversationList.some(item => item.id === '-1'))
+      return
 
-  const conversationManagement = (
-    <ConversationManagement
-      APP_ID={APP_ID}
-      promptConfig={promptConfig}
-      inited={inited}
-      currConversationId={currConversationId}
-      setCurrConversationId={setCurrConversationId}
-      setChatList={setChatList}
-      getChatList={getChatList}
-      setNewConversationInfo={setNewConversationInfo}
-      setExistConversationInfo={setExistConversationInfo}
-      resetNewConversationInputs={resetNewConversationInputs}
-      isNewConversation={isNewConversation}
-      currConversationInfo={currConversationInfo}
-      currInputs={currInputs}
-      newConversationInputs={newConversationInputs}
-      setCurrInputs={setCurrInputs}
-      isShowPrompt={isShowPrompt}
-      setConversationList={setConversationList}
-      isResponding={isResponding}
-    />
-  )
+    setConversationList(produce(conversationList, (draft) => {
+      draft.unshift({
+        id: '-1',
+        name: t('app.chat.newChatDefaultName'),
+        inputs: newConversationInputs,
+        introduction: conversationIntroduction,
+      })
+    }))
+  }
+
+  // sometime introduction is not applied to state
+  const generateNewChatListWithOpenStatement = (introduction?: string, inputs?: Record<string, any> | null) => {
+    let calculatedIntroduction = introduction || conversationIntroduction || ''
+    const calculatedPromptVariables = inputs || currInputs || null
+    if (calculatedIntroduction && calculatedPromptVariables)
+      calculatedIntroduction = replaceVarWithValues(calculatedIntroduction, promptConfig?.prompt_variables || [], calculatedPromptVariables)
+
+    const openStatement = {
+      id: `${Date.now()}`,
+      content: calculatedIntroduction,
+      isAnswer: true,
+      feedbackDisabled: true,
+      isOpeningStatement: isShowPrompt,
+    }
+    if (calculatedIntroduction)
+      return [openStatement]
+
+    return []
+  }
 
   // init
   useEffect(() => {
@@ -340,14 +436,22 @@ const Main: FC = () => {
           questionItem,
         })
       },
-
       async onCompleted(hasError?: boolean) {
         if (hasError)
           return
 
-        const { handleNewConversation } = conversationManagement
-        await handleNewConversation()
+        if (getConversationIdChangeBecauseOfNew()) {
+          const { data: allConversations }: any = await fetchConversations()
+          const newItem: any = await generationConversationName(allConversations[0].id)
 
+          const newAllConversations = produce(allConversations, (draft: any) => {
+            draft[0].name = newItem.name
+          })
+          setConversationList(newAllConversations as any)
+        }
+        setConversationIdChangeBecauseOfNew(false)
+        resetNewConversationInputs()
+        setChatNotStarted()
         setCurrConversationId(tempNewConversationId, APP_ID, true)
         setRespondingFalse()
         stopAutoRequest() // 停止自动请求
@@ -521,7 +625,7 @@ const Main: FC = () => {
     return (
       <Sidebar
         list={conversationList}
-        onCurrentIdChange={conversationManagement.handleConversationIdChange}
+        onCurrentIdChange={handleConversationIdChange}
         currentId={currConversationId}
         copyRight={APP_INFO.copyright || APP_INFO.title}
       />
@@ -540,7 +644,7 @@ const Main: FC = () => {
         title={APP_INFO.title}
         isMobile={isMobile}
         onShowSideBar={showSidebar}
-        onCreateNewChat={() => conversationManagement.handleConversationIdChange('-1')}
+        onCreateNewChat={() => handleConversationIdChange('-1')}
       />
       <div className="flex rounded-t-2xl bg-white overflow-hidden">
         {/* sidebar */}
@@ -558,19 +662,19 @@ const Main: FC = () => {
         {/* main */}
         <div className='flex-grow flex flex-col h-[calc(100vh_-_3rem)] overflow-y-auto'>
           <ConfigSence
-            conversationName={conversationManagement.conversationName}
-            hasSetInputs={conversationManagement.hasSetInputs}
+            conversationName={conversationName}
+            hasSetInputs={hasSetInputs}
             isPublicVersion={isShowPrompt}
             siteInfo={APP_INFO}
             promptConfig={promptConfig}
-            onStartChat={conversationManagement.handleStartChat}
+            onStartChat={handleStartChat}
             canEditInputs={canEditInputs}
             savedInputs={currInputs as Record<string, any>}
             onInputsChange={setCurrInputs}
           ></ConfigSence>
 
           {
-            conversationManagement.hasSetInputs && (
+            hasSetInputs && (
               <div className='relative grow h-[200px] pc:w-[794px] max-w-full mobile:w-full pb-[66px] mx-auto mb-3.5 overflow-hidden'>
                 <div className='h-full overflow-y-auto' ref={chatListDomRef}>
                   <Chat
