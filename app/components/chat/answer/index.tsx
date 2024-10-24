96c22be1d58a4ec14b944dfe64b0208c0f347a89
skywolf <skywolf@skywolf.com>
2024-10-24 11:42:52 +0800

    3次修正复制按钮颜色

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -49,13 +49,13 @@ export const EditIconSolid: FC<{ className?: string }> = ({ className }) => {
 }
 
 const ClipboardIcon: FC<{ className?: string }> = ({ className }) => {
-  return <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${className}`} fill="none" viewBox="0 0 24 24" strokeWidth="1">
+  return <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${className}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="1">
     <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
   </svg>
 }
 
 const ClipboardCheckIcon: FC<{ className?: string }> = ({ className }) => {
-  return <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${className}`} fill="none" viewBox="0 0 24 24" strokeWidth="1">
+  return <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${className}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="1">
     <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
   </svg>
 }
