847f1ae2a689ccbf20e7d0b0d543bc2848b92fff
skywolf <skywolf@skywolf.com>
2024-10-24 09:17:06 +0800

    2次修正复制图标

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -48,6 +48,12 @@ export const EditIconSolid: FC<{ className?: string }> = ({ className }) => {
   </svg>
 }
 
+const ClipboardCheckIcon: FC<{ className?: string }> = ({ className }) => {
+  return <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+  </svg>
+}
+
 const IconWrapper: FC<{ children: React.ReactNode | string }> = ({ children }) => {
   return <div className={'rounded-lg h-6 w-6 flex items-center justify-center hover:bg-gray-100'}>
     {children}
@@ -179,7 +185,7 @@ const Answer: FC<IAnswerProps> = ({
       <div className={`${s.itemOperation} flex gap-2`}>
         {/* <Tooltip selector={`copy-button-${randomString(16)}`} content={isCopied ? t('common.operation.copied') as string : t('common.operation.copy') as string}> */}
         <Tooltip selector={`copy-button-${randomString(16)}`} content={'复制'}>
-          {OperationBtn({ innerContent: <IconWrapper>{isCopied ? <ClipboardDocumentCheckIcon className="w-4 h-4" /> : <ClipboardIcon className="w-4 h-4" />}</IconWrapper>, onClick: handleCopy })}
+          {OperationBtn({ innerContent: <IconWrapper>{isCopied ? <ClipboardCheckIcon className="w-4 h-4" /> : <ClipboardIcon className="w-4 h-4" />}</IconWrapper>, onClick: handleCopy })}
         </Tooltip>
         {/* {userOperation()} */}
       </div>
