7d51b73340dcd105a70468d7ba9217f7f51b23b6
skywolf <skywolf@skywolf.com>
2024-11-11 11:49:06 +0800

    2次修正

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -40,15 +40,17 @@ const Sidebar: FC<ISidebarProps> = ({
     <div
       className="shrink-0 flex flex-col bg-white pc:w-[244px] tablet:w-[192px] mobile:w-[240px]  border-r border-gray-200 tablet:h-[calc(100vh_-_3rem)] mobile:h-screen"
     >
-      {list.length < MAX_CONVERSATION_LENTH && (
         <div className="flex flex-shrink-0 p-4 !pb-0">
           <Button
             onClick={() => { onCurrentIdChange('-1') }}
-            className="group block w-full flex-shrink-0 !justify-start !h-9 text-primary-600 items-center text-sm">
-            <PencilSquareIcon className="mr-2 h-4 w-4" /> {t('app.chat.newChat')}
+            className={classNames(
+              list.length >= MAX_CONVERSATION_LENTH
+                ? 'bg-gray-200 text-gray-400 cursor-not-allowed'
+                : 'text-primary-600',
+              'group block w-full flex-shrink-0 !justify-start !h-9 items-center text-sm',)}>
+            <PencilSquareIcon className="mr-2 h-4 w-4" /> {t('app.chat.newChat')}【{list.length} / {MAX_CONVERSATION_LENTH}】
           </Button>
         </div>
-      )}
 
       <nav className="mt-4 flex-1 space-y-1 bg-white p-4 !pt-0 overflow-y-auto ">
         {list.map((item) => {
