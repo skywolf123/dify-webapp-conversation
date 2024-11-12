614b8d224b838cc876e37a1693405a5990a4d040
skywolf <skywolf@skywolf.com>
2024-11-12 16:33:51 +0800

    8次修正

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -316,7 +316,7 @@ const Answer: FC<IAnswerProps> = ({
               {/* User feedback must be displayed */}
               {!feedbackDisabled && renderFeedbackRating(feedback?.rating)}
             </div>
-            <div className="absolute left-[-50px] bottom-0 flex flex-col gap-2">
+            <div className="absolute left-[-60px] bottom-0 flex flex-col justify-end gap-2">
               {!feedbackDisabled && !item.feedbackDisabled && renderItemOperation()}
               {/* User feedback must be displayed */}
               {!feedbackDisabled && renderFeedbackRating(feedback?.rating)}
