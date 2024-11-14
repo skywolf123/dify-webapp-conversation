bdb487e8da1e38e100de54b5d1f1657fa2207004
skywolf <skywolf@skywolf.com>
2024-11-14 11:30:11 +0800

    更新md样式

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: '便利AI写 v0.1.0',
+  title: '便利AI写 v0.1.1',
   description: '',
   copyright: '',
   privacy_policy: '',
